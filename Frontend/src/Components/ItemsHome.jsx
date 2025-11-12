f8c37269fa1f179df6a4da9906fddf33a7fe1cae
namanydv <namanofficial91@gmail.com>
2025-11-12 10:02:29 +0530

    feat: add sidebar structure to ItemsHome component for improved layout

--- Frontend/src/Components/ItemsHome.jsx
+++ Frontend/src/Components/ItemsHome.jsx
@@ -12,6 +12,21 @@ const ItemsHome = () => {
   return (
     <div className={itemsHomeStyles.page}>
       <BannerHome onSearch={handleSearch}/>
+
+      <div className='flex flex-col lg:flex-row flex-1'>
+        <aside className={itemsHomeStyles.sidebar}>
+          <div className={itemsHomeStyles.sidebarHeader}>
+            <h1 
+            style={{
+              fontFamily:" 'Playfair Display', serif ",
+              textShadow: " 2px 2px 4px rgba(0,0,0,0.2) "
+            }}
+            className={itemsHomeStyles.sidebarTitle}>
+                FreshCart
+            </h1>
+          </div>
+        </aside>
+      </div>
     </div>
   )
 }
