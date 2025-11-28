183145670be6112182ba899f00cec719c17dd8e0
namanydv <namanofficial91@gmail.com>
2025-11-28 22:48:53 +0530

    fix: add missing container and grid structure in Footer component

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -29,6 +29,12 @@ const Footer = () => {
         <div className={`${footerStyles.floatingShape} -top-24 -right-24 w-80 h-80 opacity-20`}></div>
         <div className={`${footerStyles.floatingShape} -bottom-40 -left-24 w-96 h-96 opacity-15 animation-delay-2000`}></div>
         <div className={`${footerStyles.floatingShape} top-1/4 left-1/3 w-64 h-64 bg-emerald-600 opacity-10 animate-pulse animation-delay-1000`}></div>
+
+        <div className={footerStyles.container}>
+          <div className={footerStyles.grid}>
+            {/* BRAND */}
+          </div>
+        </div>
     </footer>
   )
 }
